917bee5839f11e76b532eaa924087b84cdef0aff
xiao1-gpi <2577993845@qq.COM>
2026-07-03 22:06:57 +0800

    fix: 修复部署构建错误，移除clsx和tailwind-merge依赖

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+type ClassValue =
+  | string
+  | number
+  | null
+  | false
+  | undefined
+  | ClassValue[]
+  | Record<string, boolean | null | undefined>;
+
+function toVal(mix: ClassValue): string {
+  let k: string;
+  let y: string;
+  let str = '';
+
+  if (typeof mix === 'string' || typeof mix === 'number') {
+    str += mix;
+  } else if (typeof mix === 'object') {
+    if (Array.isArray(mix)) {
+      for (let i = 0; i < mix.length; i++) {
+        if (mix[i]) {
+          y = toVal(mix[i]);
+          if (y) str && (str += ' '), str += y;
+        }
+      }
+    } else if (mix) {
+      for (k in mix) {
+        if (mix[k]) {
+          str && (str += ' '), str += k;
+        }
+      }
+    }
+  }
+
+  return str;
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  let i = 0;
+  let tmp: ClassValue;
+  let x: string;
+  let str = '';
+  const len = inputs.length;
+  for (; i < len; i++) {
+    tmp = inputs[i];
+    if (tmp) {
+      x = toVal(tmp);
+      if (x) str && (str += ' '), str += x;
+    }
+  }
+  return str;
 }
